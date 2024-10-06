8e221b45471ca33dd86ff50814146d07c2ef7955
Ismytv <ismytv@gmail.com>
2024-10-05 21:33:27 -0500

    feat(auth):refactoriza middleware, solo como demo con validacion dura

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,22 +5,31 @@ const privateRoutes = ["/protected"];
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
   console.log("Middleware executed");
-  console.log(context.url);
+  //console.log(context.url);
 
   const authHeaders = context.request.headers.get("authorization");
   console.log("authHeaders", authHeaders);
 
-  if (privateRoutes.includes(context.url.pathname)) {
-    if (authHeaders) return next();
-    else {
-      return new Response("Unauthorized", {
-        status: 401,
-        headers: {
-          "WWW-Authenticate": 'Basic realm="Secure Area"',
-        },
-      });
-    }
-  }
+  if (privateRoutes.includes(context.url.pathname))
+    return checkLocalAuth(authHeaders, next);
 
   return next();
 });
+
+const checkLocalAuth = (authHeaders: string | null, next: any) => {
+  if (authHeaders) {
+    const authValue = authHeaders.split(" ").at(-1) ?? "user:pass";
+    const decodeValue = atob(authValue).split(":");
+    const [user, pass] = decodeValue;
+
+    if (user == "user" && pass == "pass") return next();
+  }
+  return new Response("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+
+  });
+  return next();
+};
