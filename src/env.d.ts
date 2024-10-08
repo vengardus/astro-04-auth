1f76bd857852563e6fbef36fe8051241c2998835
Ismytv <ismytv@gmail.com>
2024-10-08 15:39:48 -0500

    feat(auth):protege rutas

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 
+interface User {
+  name: string;
+  email: string;
+  photoURL: string;
+  emailVerified: boolean;
+} 
+
 declare namespace App {
   interface Locals {
     isLoggedIn: boolean;
+    user: User | null
   }
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 import { firebase } from "./firebase/config";
 
 const privateRoutes = ["/protected"];
+const notAuthenticatedRoutes = ["/auth/login", "/auth/register"];
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
@@ -12,6 +13,22 @@ export const onRequest = defineMiddleware((context, next) => {
   const user = firebase.auth.currentUser;
 
   context.locals.isLoggedIn = isLoggedIn;   // definida en env.d.ts
+  if ( user ) {
+    context.locals.user = {
+      name: user.displayName!,
+      email: user.email!,
+      photoURL: user.photoURL?? '',
+      emailVerified: user.emailVerified
+    }
+  }
+
+  if ( !isLoggedIn && privateRoutes.includes(context.url.pathname)) {
+    return context.redirect("/");
+  }
+  
+  if (isLoggedIn && notAuthenticatedRoutes.includes(context.url.pathname)) {
+    return context.redirect("/");
+  }
 
   return next();
 });
