c8209226be97da9af0de206574fd2083e8d04555
Ismytv <ismytv@gmail.com>
2024-10-08 18:00:23 -0500

    fix(Navbar):script no se ejecutaba o no se volvía acargar al cambiar de página. Seagregó un observador DOM (MutationObserver)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
+
+
+import vercel from '@astrojs/vercel/serverless';
+
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel(),
 });
