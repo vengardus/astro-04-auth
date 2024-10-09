20507b4ca8789e3c31ba8ee4676ca347323e7c7c
Ismytv <ismytv@gmail.com>
2024-10-08 22:58:44 -0500

    deploy(tester03)

--- src/actions/app/get-env.action.ts
+++ src/actions/app/get-env.action.ts
@@ -5,7 +5,7 @@ export const getEnv_ = async (key:string):Promise<ResponseAction> => {
     const resp = initResponseAction();
     console.log('get-key', key)
     const value = import.meta.env[key]?? ''
-    console.log('value-key', value)
+    console.log('value-key', import.meta.env.FIREBASE_API_KEY)
     resp.success = true
     resp.data = value
 
