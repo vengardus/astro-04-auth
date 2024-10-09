5ca32956f8a192ee705cb4f3a1ff2372b3e59635
Ismytv <ismytv@gmail.com>
2024-10-09 16:59:31 -0500

    chore:agrega Auth.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,13 @@ import db from '@astrojs/db';
 
 
 
+import auth from 'auth-astro';
+
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()],
+  integrations: [tailwind(), db(), auth()],
   output: 'server',
   adapter: vercel(),
 });
